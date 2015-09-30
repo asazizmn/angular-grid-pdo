a1e3e969a04bac4b7087af06ed8a57f6cd5df4e5
Aziz <azizsaboor@gmail.com>
2015-09-30 08:54:39 +0800

    - config update

--- includes/config.php
+++ includes/config.php
@@ -7,9 +7,9 @@
  */
 function connect() 
 {
-    $host = 'localhost';
-    $db_name = 'angularcode_grid';
-    $db_user = 'root';
+    $host = '';
+    $db_name = '';
+    $db_user = '';
     $db_password = '';
     
     return new PDO('mysql:host='.$host.';dbname='.$db_name, $db_user, $db_password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
